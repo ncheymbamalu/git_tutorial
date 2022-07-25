71e9eb8517382df3642da4d6aef76003fe24d2f8
ncheymbamalu <nchey.learnings@gmail.com>
2022-07-25 08:50:55 -0700

    added comments and an output string to the add function

--- math.py
+++ math.py
@@ -1,8 +1,10 @@
 # I need to implement this
 def add(x, y):
+  # if x and y are integers, then add them
   if type(x) == int and type(y) == int:
     return x + y
-  return None
+  # otherwise return the below string
+  return 'Nope, both inputs must be integers!'
 
 # Jane Doe, my imaginary team member, needs to implement this
 def multiply(x, y):
